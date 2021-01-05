52d2ddb371408265e81ee47603e607387b801581
Christopher Iliyas <christopheriliyas@gmail.com>
2021-01-05 21:15:38 +0100

    add education and work templates

--- lpc/src/app/app.module.ts
+++ lpc/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { InfoPageComponent } from './info-page/info-page.component';
+import { EducationPageComponent } from './education-page/education-page.component';
+import { WorkPageComponent } from './work-page/work-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InfoPageComponent
+    InfoPageComponent,
+    EducationPageComponent,
+    WorkPageComponent
   ],
   imports: [
     BrowserModule
