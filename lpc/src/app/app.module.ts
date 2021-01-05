8f60094ecc37a8732c5605d2c119e6349197cc02
Christopher Iliyas <christopheriliyas@gmail.com>
2021-01-05 21:47:07 +0100

    refactor global styles

--- lpc/src/app/app.module.ts
+++ lpc/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { InfoPageComponent } from './info-page/info-page.component';
 import { EducationPageComponent } from './education-page/education-page.component';
 import { WorkPageComponent } from './work-page/work-page.component';
+import { PhotoPageComponent } from './photo-page/photo-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InfoPageComponent,
     EducationPageComponent,
-    WorkPageComponent
+    WorkPageComponent,
+    PhotoPageComponent
   ],
   imports: [
     BrowserModule
